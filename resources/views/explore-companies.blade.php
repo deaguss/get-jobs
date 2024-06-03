3701ec72fe32b63f5cd6cef03ebce170f9116fbf
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-06-03 18:54:11 +0800

    Detail Company Profile

--- resources/views/explore-companies.blade.php
+++ resources/views/explore-companies.blade.php
@@ -76,7 +76,7 @@
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Company name</h5>
                     </a>
                     <p class="mb-3 font-normal text-justify text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, dolores. Fugiat cum, earum vitae quis ad sequi sed natus eaque.</p>
-                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#051A49] rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300">
+                    <a href="/detail-company" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#051A49] rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300">
                         See Jobs
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,14 @@ Route::get('/explore-companies', function () {
     return view('explore-companies');
 });
 
-Route::prefix('sign-up')->name('signup.')->group(function ()
-{
+Route::prefix('sign-up')->name('signup.')->group(function () {
     Route::get('/', [AuthController::class, 'showRegistrationForm'])->name('form');
     Route::post('/', [AuthController::class, 'register']);
     Route::post('/verify-otp/{id}', [AuthController::class, 'verifyOtp'])->name('verify.otp');
     Route::post('/resend-otp/{id}', [AuthController::class, 'resendOtp'])->name('resend.otp');
 });
 
-Route::prefix('sign-in')->name('signin.')->group(function ()
-{
+Route::prefix('sign-in')->name('signin.')->group(function () {
     Route::get('/', [AuthController::class, 'showLoginForm'])->name('form');
     Route::post('/', [AuthController::class, 'login']);
     Route::get('/forget-password', [ForgetPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.form');
@@ -43,9 +41,12 @@ Route::prefix('sign-in')->name('signin.')->group(function ()
 
     Route::get('/reset-password/{token}', [ForgetPasswordController::class, 'showResetPasswordForm'])->name('reset.password.form');
     Route::post('/reset-password/{token}', [ForgetPasswordController::class, 'resetPassword'])->name('reset.password');
-
 });
 
 Route::get('/sign-out', [AuthController::class, 'logout'])->name('sign.out');
 
 Route::get('/profile', [ProfileUser::class, 'index'])->name('profile');
+
+Route::get('/detail-company', function () {
+    return view('detail-company.index');
+});
